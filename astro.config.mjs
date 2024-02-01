cbe2e65c51e950423e92aa659975e048f686014f
Stefankart <102956582+Stefankart@users.noreply.github.com>
2024-02-01 10:13:40 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://astros.zank.studio",
+	site: "https://stephan-koning.pages.dev/",
 	vite: {
 		define: {
 			__DATE__: `'${new Date().toISOString()}'`,
